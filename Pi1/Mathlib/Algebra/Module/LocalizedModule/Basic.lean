347d2eeca5eb25e94406a236faf11a90938a577b
Christian Merten <christian@merten.dev>
2025-02-26 14:33:41 +0100

    bump mathlib

--- Pi1/Mathlib/Algebra/Module/LocalizedModule/Basic.lean
+++ Pi1/Mathlib/Algebra/Module/LocalizedModule/Basic.lean
@@ -12,48 +12,6 @@ lemma Module.FinitePresentation.of_equiv {R M N : Type*} [Ring R] [AddCommGroup 
     Module.FinitePresentation R N := by
   simp [← Module.FinitePresentation.fg_ker_iff e.toLinearMap e.surjective, Submodule.fg_bot]
 
-variable {R : Type*} [CommSemiring R] (S : Submonoid R) {M M' M'' : Type*}
-  [AddCommMonoid M] [AddCommMonoid M']
-  [AddCommMonoid M''] [Module R M] [Module R M'] [Module R M'']
-  (f : M →ₗ[R] M') (g : M →ₗ[R] M'') [IsLocalizedModule S f]
-
-@[simp]
-lemma IsLocalizedModule.iso_symm_apply'' (x) : (iso S f).symm (f x) = LocalizedModule.mk x 1 := by
-  show ((iso S f).symm.toLinearMap.comp f) x = _
-  --have := iso_symm_comp S f
-  rw [iso_symm_comp]
-  simp
-
-@[simp]
-lemma IsLocalizedModule.iso_mk_one (x) : (iso S f) (LocalizedModule.mk x 1) = f x := by
-  simp
-  --show ((iso S f).symm.toLinearMap.comp f) x = _
-  ----have := iso_symm_comp S f
-  --rw [iso_symm_comp]
-  --simp
-
-@[simp]
-lemma LocalizedModule.lift_mk_one (h : ∀ (x : S), IsUnit ((algebraMap R (Module.End R M'')) x))
-    (m : M) :
-    (lift S g h) (mk m 1) = g m := by
-  conv_rhs => rw [← LocalizedModule.lift_comp S g h]
-  simp
-
-@[simp]
-lemma IsLocalizedModule.lift_comp_iso
-    (h : ∀ (x : S), IsUnit ((algebraMap R (Module.End R M'')) x)) :
-    IsLocalizedModule.lift S f g h ∘ₗ iso S f = LocalizedModule.lift S g h := by
-  apply IsLocalizedModule.ext S (LocalizedModule.mkLinearMap S M) h
-  ext x
-  simp
-
-@[simp]
-lemma IsLocalizedModule.lift_iso (h : ∀ (x : S), IsUnit ((algebraMap R (Module.End R M'')) x)) (x) :
-    IsLocalizedModule.lift S f g h ((iso S f) x) =
-      LocalizedModule.lift _ g h x := by
-  rw [← IsLocalizedModule.lift_comp_iso S f]
-  dsimp
-
 end
 
 instance {R : Type*} [CommRing R] (S : Submonoid R) (M : Type*) [AddCommMonoid M] [Module R M]
@@ -93,7 +51,7 @@ instance IsLocalizedModule.prodMap {R M N M' N' : Type*} [CommSemiring R] (S : S
         ((isBaseChange S (Localization S)
             (LocalizedModule.mkLinearMap S N)).equiv.restrictScalars R ≪≫ₗ iso S g)
   have : (f.prodMap g) = e₃ ∘ₗ (TensorProduct.mk R (Localization S) (M × N) 1) := by
-    ext x : 2 <;> simp [e₃, -IsLocalizedModule.iso_apply, IsBaseChange.equiv_tmul]
+    ext x : 2 <;> simp [e₃, IsBaseChange.equiv_tmul]
   rw [this]
   infer_instance
 
