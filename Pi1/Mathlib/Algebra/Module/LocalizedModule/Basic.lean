d03c7d526227bbd4aee6975f6e63dcd30732d243
Christian Merten <christian@merten.dev>
2025-02-26 21:01:40 +0100

    polishing

--- Pi1/Mathlib/Algebra/Module/LocalizedModule/Basic.lean
+++ Pi1/Mathlib/Algebra/Module/LocalizedModule/Basic.lean
@@ -14,29 +14,96 @@ lemma Module.FinitePresentation.of_equiv {R M N : Type*} [Ring R] [AddCommGroup 
 
 end
 
-instance {R : Type*} [CommRing R] (S : Submonoid R) (M : Type*) [AddCommMonoid M] [Module R M]
-    [Subsingleton M] : Subsingleton (LocalizedModule S M) := by
-  rw [LocalizedModule.subsingleton_iff]
-  intro m
-  use 1, S.one_mem, Subsingleton.elim _ _
+section
+-- #22328
+
+variable {R : Type*} [CommSemiring R] (S : Submonoid R)
+
+/-- The localization of an `R`-module `M` at a submonoid `S` is isomorphic to `S⁻¹R ⊗[R] M` as
+an `S⁻¹R`-module. -/
+noncomputable def LocalizedModule.equivTensorProduct (M : Type*) [AddCommMonoid M] [Module R M] :
+    LocalizedModule S M ≃ₗ[Localization S] Localization S ⊗[R] M :=
+  IsLocalizedModule.isBaseChange S (Localization S)
+    (LocalizedModule.mkLinearMap S M) |>.equiv.symm
+
+@[simp]
+lemma LocalizedModule.equivTensorProduct_symm_apply_tmul
+    (M : Type*) [AddCommMonoid M] [Module R M] (x : M) (r : R)(s : S) :
+    (equivTensorProduct S M).symm (Localization.mk r s ⊗ₜ[R] x) = r • mk x s := by
+  simp [equivTensorProduct, IsBaseChange.equiv_tmul, mk_smul_mk, smul'_mk]
+
+@[simp]
+lemma LocalizedModule.equivTensorProduct_symm_apply_tmul_one
+    (M : Type*) [AddCommMonoid M] [Module R M] (x : M) :
+    (equivTensorProduct S M).symm (1 ⊗ₜ[R] x) = mk x 1 := by
+  simp [← Localization.mk_one]
+
+@[simp]
+lemma LocalizedModule.equivTensorProduct_apply_mk
+    (M : Type*) [AddCommMonoid M] [Module R M] (x : M) (s : S) :
+    equivTensorProduct S M (mk x s) = Localization.mk 1 s ⊗ₜ[R] x := by
+  apply (equivTensorProduct S M).symm.injective
+  simp
+
+end
+
+section
+-- #22339
+
+section
+
+variable {R A M M' : Type*} [CommSemiring R] [CommSemiring A] [Algebra R A] (S : Submonoid R)
+  [AddCommMonoid M] [Module R M] [AddCommMonoid M'] [Module R M']
+  [IsLocalization S A]
+
+noncomputable
+def IsLocalizedModule.module (f : M →ₗ[R] M') [IsLocalizedModule S f] : Module A M' :=
+  (IsLocalizedModule.iso S f).symm.toAddEquiv.module A
+
+instance IsLocalizedModule.module_isScalarTower (f : M →ₗ[R] M') [IsLocalizedModule S f] :
+    letI : Module A M' := IsLocalizedModule.module S f
+    IsScalarTower R A M' :=
+  (IsLocalizedModule.iso S f).symm.isScalarTower A
+
+end
+
+lemma IsBaseChange.pi {R S ι : Type*} [Finite ι] [CommSemiring R] [CommSemiring S]
+    [Algebra R S]
+    {M M' : ι → Type*} [∀ i, AddCommMonoid (M i)] [∀ i, AddCommMonoid (M' i)]
+    [∀ i, Module R (M i)] [∀ i, Module R (M' i)] [∀ i, Module S (M' i)]
+    [∀ i, IsScalarTower R S (M' i)]
+    (f : ∀ i, M i →ₗ[R] M' i) (hf : ∀ i, IsBaseChange S (f i)) :
+    IsBaseChange S (.pi fun i ↦ f i ∘ₗ .proj i) := by
+  classical
+  cases nonempty_fintype ι
+  apply IsBaseChange.of_equiv <| TensorProduct.piRight R S _ M ≪≫ₗ
+    .piCongrRight fun i ↦ (hf i).equiv
+  intro x
+  ext i
+  simp [IsBaseChange.equiv_tmul]
 
 instance IsLocalizedModule.pi {R ι : Type*} [Finite ι] [CommSemiring R] (S : Submonoid R)
     {M M' : ι → Type*} [∀ i, AddCommMonoid (M i)] [∀ i, AddCommMonoid (M' i)]
     [∀ i, Module R (M i)] [∀ i, Module R (M' i)]
     (f : ∀ i, M i →ₗ[R] M' i) [∀ i, IsLocalizedModule S (f i)] :
-    IsLocalizedModule S (LinearMap.pi fun i ↦ f i ∘ₗ LinearMap.proj i) := by
-  classical
-  cases nonempty_fintype ι
-  let e₃ : Localization S ⊗[R] (Π i, M i) ≃ₗ[R] Π i, M' i :=
-    TensorProduct.piRight R R _ M ≪≫ₗ LinearEquiv.piCongrRight
-      (fun i ↦ (isBaseChange S (Localization S)
-        (LocalizedModule.mkLinearMap S _)).equiv.restrictScalars R ≪≫ₗ iso S (f i))
-  have : ((LinearMap.pi fun i ↦ f i ∘ₗ LinearMap.proj i)) =
-      e₃ ∘ₗ (TensorProduct.mk R (Localization S) (Π i, M i) 1) := by
-    ext x
-    simp [e₃, IsBaseChange.equiv_tmul]
-  rw [this]
+    IsLocalizedModule S (.pi fun i ↦ f i ∘ₗ .proj i) := by
+  letI (i : ι) : Module (Localization S) (M' i) := IsLocalizedModule.module S (f i)
+  rw [isLocalizedModule_iff_isBaseChange S (Localization S)]
+  apply IsBaseChange.pi
+  intro i
+  rw [← isLocalizedModule_iff_isBaseChange S]
   infer_instance
+
+lemma IsBaseChange.prodMap {R S M N M' N' : Type*} [CommSemiring R] [CommSemiring S]
+    [Algebra R S] [AddCommMonoid M] [AddCommMonoid N] [Module R M] [Module R N]
+    [AddCommMonoid M'] [AddCommMonoid N'] [Module R M'] [Module R N']
+    [Module S M'] [Module S N'] [IsScalarTower R S M'] [IsScalarTower R S N']
+    (f : M →ₗ[R] M') (g : N →ₗ[R] N')
+    (hf : IsBaseChange S f) (hg : IsBaseChange S g) :
+    IsBaseChange S (f.prodMap g) := by
+  apply IsBaseChange.of_equiv (TensorProduct.prodRight R _ S M N ≪≫ₗ hf.equiv.prod hg.equiv)
+  intro p
+  simp [hf.equiv_tmul, hg.equiv_tmul]
 
 instance IsLocalizedModule.prodMap {R M N M' N' : Type*} [CommSemiring R] (S : Submonoid R)
     [AddCommMonoid M] [AddCommMonoid N] [Module R M] [Module R N]
@@ -44,16 +111,19 @@ instance IsLocalizedModule.prodMap {R M N M' N' : Type*} [CommSemiring R] (S : S
     (f : M →ₗ[R] M') (g : N →ₗ[R] N')
     [IsLocalizedModule S f] [IsLocalizedModule S g] :
     IsLocalizedModule S (f.prodMap g) := by
-  let e₃ : Localization S ⊗[R] (M × N) ≃ₗ[R] M' × N' :=
-    TensorProduct.prodRight R _ (Localization S) M N ≪≫ₗ
-        ((isBaseChange S (Localization S)
-            (LocalizedModule.mkLinearMap S M)).equiv.restrictScalars R ≪≫ₗ iso S f).prod
-        ((isBaseChange S (Localization S)
-            (LocalizedModule.mkLinearMap S N)).equiv.restrictScalars R ≪≫ₗ iso S g)
-  have : (f.prodMap g) = e₃ ∘ₗ (TensorProduct.mk R (Localization S) (M × N) 1) := by
-    ext x : 2 <;> simp [e₃, IsBaseChange.equiv_tmul]
-  rw [this]
-  infer_instance
+  letI : Module (Localization S) M' := IsLocalizedModule.module S f
+  letI : Module (Localization S) N' := IsLocalizedModule.module S g
+  rw [isLocalizedModule_iff_isBaseChange S (Localization S)]
+  apply IsBaseChange.prodMap
+  · rw [← isLocalizedModule_iff_isBaseChange S]
+    infer_instance
+  · rw [← isLocalizedModule_iff_isBaseChange S]
+    infer_instance
+
+end
+
+section
+-- #22336
 
 @[simps!]
 def LinearEquiv.extendScalarsOfIsLocalization
@@ -78,9 +148,8 @@ def LinearEquiv.extendScalarsOfIsLocalizationEquiv
   left_inv e := by ext; simp
   right_inv e := by ext; simp
 
-/-- -/
-noncomputable
-def IsLocalizedModule.mapEquiv {R : Type*} [CommSemiring R] (S : Submonoid R)
+@[simps!]
+noncomputable def IsLocalizedModule.mapEquiv {R : Type*} [CommSemiring R] (S : Submonoid R)
     (A : Type*) {M N M' N' : Type*} [CommSemiring A] [Algebra R A] [IsLocalization S A]
     [AddCommMonoid M] [AddCommMonoid N] [AddCommMonoid M'] [AddCommMonoid N']
     [Module R M] [Module R N] [Module R M'] [Module R N']
@@ -102,30 +171,4 @@ def IsLocalizedModule.mapEquiv {R : Type*} [CommSemiring R] (S : Submonoid R)
       ext
       simp)
 
-
-/-- The localization of an `R`-module `M` at a submonoid `S` is isomorphic to `S⁻¹R ⊗[R] M` as
-an `S⁻¹R`-module. -/
-noncomputable def LocalizedModule.equivTensorProduct {R : Type*} [CommSemiring R] (S : Submonoid R)
-    (M : Type*) [AddCommMonoid M] [Module R M] :
-    LocalizedModule S M ≃ₗ[Localization S] Localization S ⊗[R] M :=
-  IsLocalizedModule.isBaseChange S (Localization S)
-    (LocalizedModule.mkLinearMap S M) |>.equiv.symm
-
-@[simp]
-lemma LocalizedModule.equivTensorProduct_symm_apply_tmul {R : Type*} [CommSemiring R] (S : Submonoid R)
-    (M : Type*) [AddCommMonoid M] [Module R M] (x : M) (r : R)(s : S) :
-    (equivTensorProduct S M).symm (Localization.mk r s ⊗ₜ[R] x) = r • mk x s := by
-  simp [equivTensorProduct, IsBaseChange.equiv_tmul, mk_smul_mk, smul'_mk]
-
-@[simp]
-lemma LocalizedModule.equivTensorProduct_symm_apply_tmul_one {R : Type*} [CommSemiring R] (S : Submonoid R)
-    (M : Type*) [AddCommMonoid M] [Module R M] (x : M) :
-    (equivTensorProduct S M).symm (1 ⊗ₜ[R] x) = mk x 1 := by
-  simp [← Localization.mk_one]
-
-@[simp]
-lemma LocalizedModule.equivTensorProduct_apply_mk {R : Type*} [CommSemiring R] (S : Submonoid R)
-    (M : Type*) [AddCommMonoid M] [Module R M] (x : M) (s : S) :
-    equivTensorProduct S M (mk x s) = Localization.mk 1 s ⊗ₜ[R] x := by
-  apply (equivTensorProduct S M).symm.injective
-  simp
+end
