97e493f9add871eb25d0188e9c3f4929b4df4445
Christian Merten <christian@merten.dev>
2025-03-04 19:48:29 +0100

    flat and faithfully flat

--- Pi1/RingTheory/SmoothDescent.lean
+++ Pi1/RingTheory/SmoothDescent.lean
@@ -11,10 +11,26 @@ variable (M : Type*) [AddCommGroup M] [Module R M]
 
 /-- Flat descends along faithfully flat ring maps. -/
 lemma Flat.of_flat_tensorProduct [Module.FaithfullyFlat R S] [Module.Flat S (S ⊗[R] M)] :
-    Module.Flat R M :=
-  sorry
+    Module.Flat R M := by
+  rw [Module.Flat.iff_lTensor_preserves_injective_linearMap]
+  intro N P _ _ _ _ f hf
+  have : Flat R (S ⊗[R] M) := Flat.trans _ S _
+  rw [← FaithfullyFlat.lTensor_injective_iff_injective R S]
+  have : LinearMap.lTensor S (LinearMap.lTensor M f) =
+      (TensorProduct.assoc _ _ _ _).toLinearMap ∘ₗ LinearMap.lTensor (S ⊗[R] M) f ∘ₗ
+        (TensorProduct.assoc _ _ _ _).symm.toLinearMap := by
+    ext
+    simp
+  simpa [this] using lTensor_preserves_injective_linearMap f hf
 
-instance [Module.FaithfullyFlat R M] : Module.FaithfullyFlat S (S ⊗[R] M) := sorry
+/-- Faithful flatness is preserved by arbitrary base change. -/
+instance [Module.FaithfullyFlat R M] : Module.FaithfullyFlat S (S ⊗[R] M) := by
+  rw [Module.FaithfullyFlat.iff_flat_and_rTensor_reflects_triviality]
+  refine ⟨inferInstance, fun N _ _ hN ↦ ?_⟩
+  let _ : Module R N := Module.compHom N (algebraMap R S)
+  have : IsScalarTower R S N := IsScalarTower.of_algebraMap_smul fun r ↦ congrFun rfl
+  have := (AlgebraTensorModule.cancelBaseChange R S S N M).symm.subsingleton
+  exact FaithfullyFlat.rTensor_reflects_triviality R M N
 
 end Module
 
