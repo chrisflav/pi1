5dfbce9803f6bd9ca392d2663c3a17a21cece70f
Christian Merten <christian@merten.dev>
2025-03-12 20:20:44 +0100

    progress

--- Pi1/RingTheory/FinitePresentation.lean
+++ Pi1/RingTheory/FinitePresentation.lean
@@ -1,3 +1,4 @@
+import Mathlib.RingTheory.IsAdjoinRoot
 import Mathlib.Algebra.Module.FinitePresentation
 import Mathlib.RingTheory.FinitePresentation
 import Mathlib.RingTheory.Etale.Pi
@@ -107,38 +108,80 @@ lemma Module.FinitePresentation.of_equiv {R M N : Type*} [Ring R] [AddCommGroup 
 instance (priority := 900) Module.FinitePresentation.of_subsingleton
     {R M : Type*} [CommRing R] [AddCommGroup M] [Module R M] [Subsingleton M] :
     Module.FinitePresentation R M :=
-  sorry
+  .of_equiv (default : (Fin 0 → R) ≃ₗ[R] M)
 
-private lemma Module.FinitePresentation.pi_aux {ι : Type*} [Fintype ι] (R : Type*) (M : ι → Type*)
+universe v
+
+@[elab_as_elim]
+lemma Module.pi_induction {ι : Type} [Finite ι] (R : Type*) [Semiring R]
+    (motive : ∀ (N : Type u) [AddCommMonoid N] [Module R N], Prop)
+    (equiv : ∀ {N N' : Type u} [AddCommMonoid N] [AddCommMonoid N']
+      [Module R N] [Module R N'], (N ≃ₗ[R] N') → motive N → motive N')
+    (unit : motive PUnit)
+    (prod : ∀ {N N' : Type u} [AddCommMonoid N] [AddCommMonoid N']
+      [Module R N] [Module R N'], motive N → motive N' → motive (N × N'))
+    (M : ι → Type u) [∀ i, AddCommMonoid (M i)] [∀ i, Module R (M i)]
+    (h : ∀ i, motive (M i)) :
+    motive (∀ i, M i) := by
+  classical
+  cases nonempty_fintype ι
+  revert M
+  refine Fintype.induction_empty_option ?_ ?_ ?_ ι
+  · intro α β _ e h M _ _ hM
+    apply equiv (LinearEquiv.piCongrLeft R M e) <| h _ fun i ↦ hM _
+  · intro M _ _ _
+    exact equiv default unit
+  · intro α _ h M _ _ hn
+    exact equiv (LinearEquiv.piOptionEquivProd R).symm <| prod (hn _) (h _ fun i ↦ hn i)
+
+@[elab_as_elim]
+lemma Module.pi_induction' {ι : Type} [Finite ι] (R : Type*) [CommRing R]
+    (motive : ∀ (N : Type u) [AddCommGroup N] [Module R N], Prop)
+    (equiv : ∀ {N N' : Type u} [AddCommGroup N] [AddCommGroup N']
+      [Module R N] [Module R N'], (N ≃ₗ[R] N') → motive N → motive N')
+    (unit : motive PUnit)
+    (prod : ∀ {N N' : Type u} [AddCommGroup N] [AddCommGroup N']
+      [Module R N] [Module R N'], motive N → motive N' → motive (N × N'))
+    (M : ι → Type u) [∀ i, AddCommGroup (M i)] [∀ i, Module R (M i)]
+    (h : ∀ i, motive (M i)) :
+    motive (∀ i, M i) := by
+  classical
+  cases nonempty_fintype ι
+  revert M
+  refine Fintype.induction_empty_option ?_ ?_ ?_ ι
+  · intro α β _ e h M _ _ hM
+    apply equiv (LinearEquiv.piCongrLeft R M e) <| h _ fun i ↦ hM _
+  · intro M _ _ _
+    exact equiv default unit
+  · intro α _ h M _ _ hn
+    exact equiv (LinearEquiv.piOptionEquivProd R).symm <| prod (hn _) (h _ fun i ↦ hn i)
+
+instance Module.FinitePresentation.prod (R : Type*) (M N : Type*)
+    [CommRing R] [AddCommGroup M] [AddCommGroup N] [Module R M] [Module R N]
+    [Module.FinitePresentation R M] [Module.FinitePresentation R N] :
+    Module.FinitePresentation R (M × N) := by
+  have hf : Function.Surjective (LinearMap.fst R M N) := LinearMap.fst_surjective
+  have : FinitePresentation R ↥(LinearMap.ker (LinearMap.fst R M N)) := by
+    rw [LinearMap.ker_fst]
+    exact .of_equiv (LinearEquiv.ofInjective (LinearMap.inr R M N) LinearMap.inr_injective)
+  apply Module.finitePresentation_of_ker (.fst R M N) hf
+
+private lemma Module.FinitePresentation.pi_aux {ι : Type} [Finite ι] (R : Type*) (M : ι → Type*)
     [CommRing R] [∀ i, AddCommGroup (M i)] [∀ i, Module R (M i)]
     [∀ i, Module.FinitePresentation R (M i)] : Module.FinitePresentation R (∀ i, M i) := by
-  revert R M
-  refine Fintype.induction_empty_option ?_ ?_ ?_ ι
-  · intro α β _ e h R M _ _ _ _
-    let eq : (∀ i, M i) ≃ₗ[R] ∀ j, M (e j) :=
-      sorry
-    apply Module.FinitePresentation.of_equiv eq.symm
-  · intro R M _ _ _ _
+  refine Module.pi_induction' (motive := fun N _ _ ↦ Module.FinitePresentation R N) R ?_ ?_ ?_ M
+      inferInstance
+  · intro N N' _ _ _ _ e hN
+    exact Module.FinitePresentation.of_equiv e
+  · infer_instance
+  · introv hN hN'
     infer_instance
-  · intro α _ h R M _ _ _ _
-    let f : (∀ i, M i) →ₗ[R] ∀ i, M (some i) := .pi (fun i ↦ .proj (some i))
-    let g : M none →ₗ[R] ∀ i, M i :=
-      sorry
-    let eq : LinearMap.ker f ≃ₗ[R] M none :=
-      sorry
-    have hf : Function.Surjective f := by
-      intro x
-      use fun i ↦ i.recOn 0 (fun a ↦ x a)
-      ext
-      simp [f]
-    have : FinitePresentation R (LinearMap.ker f) := .of_equiv eq.symm
-    apply Module.finitePresentation_of_ker f hf
 
-instance Module.FinitePresentation.pi {R ι : Type*} (M : ι → Type*) [CommRing R]
+instance Module.FinitePresentation.pi {R ι : Type*} [CommRing R] (M : ι → Type*)
     [∀ i, AddCommGroup (M i)] [∀ i, Module R (M i)] [∀ i, Module.FinitePresentation R (M i)]
-    [Finite ι] :
-    Module.FinitePresentation R (∀ i, M i) := by
+    [Finite ι] : Module.FinitePresentation R (∀ i, M i) := by
   cases nonempty_fintype ι
+  convert Module.FinitePresentation.of_equiv (LinearEquiv.piCongrLeft R M (Fintype.equivFin ι).symm)
   apply Module.FinitePresentation.pi_aux
 
 lemma Module.FinitePresentation.trans {R : Type*} (S : Type*) [CommRing R] [CommRing S]
@@ -170,6 +213,203 @@ lemma MvPolynomial.aeval_polynomialEval₂_C {R S : Type*} [CommSemiring R]
   | h_C => simp
   | h_monomial => simp
   | h_add p q hp hq => simp [hp, hq]
+
+noncomputable
+def Polynomial.toMvPolynomial {R ι : Type*} [CommSemiring R]
+    (i : ι) (p : R[X]) : MvPolynomial ι R :=
+  MvPolynomial.rename (fun _ : Unit ↦ i) (MvPolynomial.pUnitAlgEquiv R |>.symm <| p)
+
+lemma Module.Free.trans (R S M : Type*) [CommSemiring R] [CommSemiring S] [Algebra R S]
+    [AddCommMonoid M] [Module R M] [Module S M] [IsScalarTower R S M]
+    [Module.Free R S] [Module.Free S M] :
+    Module.Free R M :=
+  sorry
+
+lemma AdjoinRoot.free_of_monic {R : Type*} [CommRing R] {f : R[X]} (hf : f.Monic) :
+    Module.Free R (AdjoinRoot f) :=
+  .of_basis (AdjoinRoot.powerBasis' hf).basis
+
+lemma Polynomial.free_quotient_of_monic {R : Type*} [CommRing R] {p : R[X]} (hp : p.Monic) :
+    Module.Free R (R[X] ⧸ Ideal.span {p}) :=
+  AdjoinRoot.free_of_monic hp
+
+--def fooEquiv {R ι : Type*} [CommRing R] (p : Option ι → R[X]) :
+--    AdjoinRoot
+
+lemma AdjoinRoot.lift_algebraMap {R S : Type*} [CommRing R] [CommRing S] [Algebra R S]
+    (f : Polynomial R) (x : S) (hfx : Polynomial.aeval x f = 0) (r : R) :
+    AdjoinRoot.lift (algebraMap R S) x hfx (algebraMap R (AdjoinRoot f) r) =
+      algebraMap R S r := by
+  simp
+
+set_option maxHeartbeats 0 in
+set_option synthInstance.maxHeartbeats 0 in
+lemma MvPolynomial.freeQuotientOfMonic {R ι : Type*} [Finite ι] [CommRing R] (p : ι → R[X])
+    (hp : ∀ i, (p i).Monic) :
+    Module.Free R
+      (MvPolynomial ι R ⧸ (Ideal.span <| Set.range fun i ↦ (p i).toMvPolynomial i)) := by
+  cases nonempty_fintype ι
+  revert p
+  refine Fintype.induction_empty_option ?_ ?_ ?_ ι
+  · sorry
+  · intro p _
+    let e :
+        (MvPolynomial PEmpty R ⧸ Ideal.span (Set.range fun i : PEmpty ↦ (p i).toMvPolynomial i))
+          ≃ₐ[R] R :=
+      sorry
+    apply Module.Free.of_equiv e.toLinearEquiv.symm
+  · intro α _ hp q hq
+    let A :=
+      MvPolynomial α R ⧸ (Ideal.span <| Set.range fun i : α ↦ (q i).toMvPolynomial i)
+    let B := MvPolynomial (Option α) R ⧸ (Ideal.span <| Set.range fun i ↦ (q i).toMvPolynomial i)
+    let f : A →ₐ[R] B :=
+      Ideal.Quotient.liftₐ _ (MvPolynomial.aeval fun x ↦ Ideal.Quotient.mk _ <| X (some x)) sorry
+    have : Module.Free R A := by
+      apply hp
+      intro i
+      exact hq i
+    let _ : Algebra A B := f.toRingHom.toAlgebra
+    let P : A[X] := (q none).map (algebraMap R A)
+    let u : AdjoinRoot P →ₐ[A] B :=
+      AdjoinRoot.liftHom _
+        (Ideal.Quotient.mk _ (toMvPolynomial none (q none)))
+        sorry
+    let v' : B →ₐ[R] AdjoinRoot P :=
+      Ideal.Quotient.liftₐ _
+        (MvPolynomial.aeval (fun x ↦ x.elim (AdjoinRoot.root P)
+          (fun i ↦ AdjoinRoot.mk _ (Polynomial.C <| Ideal.Quotient.mk _ (X i)))))
+        sorry
+    let v : B →ₐ[A] AdjoinRoot P :=
+      { __ := v'.toRingHom
+        commutes' := sorry }
+    have h1 : v.comp u = AlgHom.id A (AdjoinRoot P) := by
+      apply AdjoinRoot.algHom_ext
+      simp [u, v, v']
+      rw [Ideal.Quotient.liftₐ_apply]
+      have : Polynomial.eval₂ (algebraMap R (AdjoinRoot P)) (AdjoinRoot.root P) (q none) = AdjoinRoot.root P := sorry
+      simp [toMvPolynomial, MvPolynomial.aeval_rename, this]
+    have h2 : ((u.comp v).restrictScalars R) =
+        ((AlgHom.id A B).restrictScalars R) := by
+      apply Ideal.Quotient.algHom_ext
+      apply MvPolynomial.algHom_ext
+      intro i
+      simp [u, v, v']
+      rw [Ideal.Quotient.liftₐ_apply]
+      simp [Ideal.Quotient.mkₐ]
+      cases i
+      · simp [P]
+        simp [toMvPolynomial]
+        sorry
+      · simp [RingHom.algebraMap_toAlgebra, f]
+        rw [Ideal.Quotient.liftₐ_apply]
+        rw [Ideal.Quotient.lift_mk]
+        simp
+        rfl
+    /-
+    let h : IsAdjoinRoot B P :=
+      { map := (Polynomial.aeval <| Ideal.Quotient.mk _ (toMvPolynomial none (q none))).toRingHom
+        map_surjective x := by
+          obtain ⟨x, rfl⟩ := Ideal.Quotient.mk_surjective x
+          induction x using MvPolynomial.induction_on with
+          | h_C r =>
+            use Polynomial.C (algebraMap R A r)
+            simp
+            erw [Polynomial.aeval_C]
+            rw [RingHom.algebraMap_toAlgebra]
+            simp
+            rfl
+          | h_X => sorry
+          | h_add p q hp hq =>
+            obtain ⟨a, ha⟩ := hp
+            obtain ⟨b, hb⟩ := hq
+            use a + b
+            simp at ha hb
+            simp [ha, hb]
+        ker_map := sorry
+        algebraMap_eq := by
+          ext
+          simp
+          sorry
+          sorry
+      }
+    #exit
+    -/
+    let e : AdjoinRoot P ≃ₐ[A] B :=
+      sorry
+      /-
+      { __ := u
+        invFun := v
+        left_inv x := by
+          induction' x using AdjoinRoot.induction_on with p
+          simp [u, v]
+          induction p using Polynomial.induction_on with
+          | h_C a =>
+            obtain ⟨a, rfl⟩ := Ideal.Quotient.mk_surjective a
+            simp [RingHom.algebraMap_toAlgebra, f, AdjoinRoot.mk, AdjoinRoot]
+            nth_rw 2 [Ideal.Quotient.liftₐ_apply]
+            simp
+            induction a using MvPolynomial.induction_on with
+            | h_C r =>
+              have : IsScalarTower R A (A[X] ⧸ Ideal.span {P}) := sorry
+              simp
+              rw [IsScalarTower.algebraMap_apply R A]
+              simp [A]
+              rfl
+            | h_X p i hp =>
+              simp [hp]
+              rw [Ideal.Quotient.liftₐ_apply]
+              have : (Ideal.Quotient.mk (Ideal.span {P}))
+                  (Polynomial.C
+                    ((Ideal.Quotient.mk
+                      (Ideal.span (Set.range fun i ↦ toMvPolynomial i (q (some i))))) p)) = 0 :=
+                sorry
+              simp [this]
+            | h_add p q hp hq =>
+              simp_rw [map_add, hp, hq]
+          | h_add p q hp hq =>
+            simp [hp, hq]
+          | h_monomial n a hn =>
+            simp at hn
+            simp [pow_add, hn]
+            rw [← mul_assoc, hn]
+            rw [Ideal.Quotient.liftₐ_apply]
+            simp [Polynomial.toMvPolynomial, MvPolynomial.aeval_rename]
+            have : (algebraMap R (AdjoinRoot P)) ((q none).coeff 0) = AdjoinRoot.root P := sorry
+            rw [← mul_assoc, this]
+        right_inv x := by
+          simp [u, v]
+          obtain ⟨x, rfl⟩ := Ideal.Quotient.mk_surjective x
+          rw [Ideal.Quotient.liftₐ_apply]
+          simp
+          induction x using MvPolynomial.induction_on with
+          | h_C r =>
+              simp
+              rw [IsScalarTower.algebraMap_apply R A]
+              simp
+              rw [IsScalarTower.algebraMap_apply R (MvPolynomial α R) A]
+              simp [RingHom.algebraMap_toAlgebra, f]
+              rw [Ideal.Quotient.liftₐ_apply]
+              rw [Ideal.Quotient.lift_mk]
+              simp
+              rfl
+          | h_add p q hp hq =>
+              simp [hp, hq]
+          | h_X p n hp =>
+              simp at hp
+              simp [hp]
+              congr
+              cases n
+              · simp [P]
+                sorry
+              · simp
+                sorry
+        }
+      -/
+    have : Module.Free A (AdjoinRoot P) := by
+      apply AdjoinRoot.free_of_monic
+      exact (hq none).map _
+    have : Module.Free A B := Module.Free.of_equiv e.toLinearEquiv
+    apply Module.Free.trans R A B
 
 instance (priority := 900) {R S : Type*} [CommRing R] [CommRing S] [Algebra R S]
     [Module.Finite R S] [Algebra.FinitePresentation R S] : Module.FinitePresentation R S := by
