adea6fa5ee4ee3c223ed9053b71213db6b70ceb7
Christian Merten <christian@merten.dev>
2025-04-25 19:11:04 +0200

    fill sorry

--- Pi1/Mathlib/RingTheory/RingHom/Etale.lean
+++ Pi1/Mathlib/RingTheory/RingHom/Etale.lean
@@ -7,6 +7,41 @@ import Pi1.Mathlib.RingTheory.RingHom.And
 
 universe u
 
+namespace Algebra
+
+@[nontriviality]
+instance {R S : Type*} [CommRing R] [CommRing S] [Algebra R S] [Subsingleton S]
+    (n : ℕ) : IsStandardSmoothOfRelativeDimension n R S := by
+  let P' : Generators R S := .ofSurjective (fun _ : Fin (n + 1) ↦ 0) <| by
+    intro s
+    exact ⟨0, Subsingleton.elim _ _⟩
+  have hker : P'.ker = ⊤ := le_antisymm le_top (fun x _ ↦ Subsingleton.elim _ _)
+  let P : SubmersivePresentation R S :=
+    { __ := P'
+      rels := Unit
+      relation _ := 1
+      span_range_relation_eq_ker := by simp [hker]
+      map _ := (0 : Fin (n + 1))
+      map_inj _ _ _ := rfl
+      relations_finite := inferInstance
+      jacobian_isUnit := isUnit_of_subsingleton _
+      isFinite := ⟨inferInstanceAs <| Finite (Fin (n + 1)), inferInstance⟩ }
+  refine ⟨⟨P, ?_⟩⟩
+  simp [P, P', Presentation.dimension, Generators.ofSurjective_vars]
+
+instance (priority := low) {R S : Type*} [CommRing R] [CommRing S] [Algebra R S]
+    [IsStandardSmooth R S] [FormallyUnramified R S] :
+    IsStandardSmoothOfRelativeDimension 0 R S := by
+  nontriviality S
+  obtain ⟨P⟩ := IsStandardSmooth.out (R := R) (S := S)
+  have : Module.rank S (Ω[S⁄R]) = P.dimension := P.rank_kaehlerDifferential
+  have : Subsingleton (Ω[S⁄R]) := inferInstance
+  refine ⟨P, ?_⟩
+  apply Nat.cast_injective (R := Cardinal)
+  rw [← P.rank_kaehlerDifferential, rank_subsingleton', CharP.cast_eq_zero]
+
+end Algebra
+
 namespace RingHom
 
 /-- A ring hom `R →+* S` is etale, if `S` is an etale `R`-algebra. -/
@@ -68,11 +103,33 @@ lemma Etale.stableUnderComposition : StableUnderComposition Etale := by
   · exact FormallyUnramified.stableUnderComposition
   · exact Smooth.stableUnderComposition
 
--- this follows from the analogous statement for smoothness and is done in a branch of mathlib
--- TODO: integrate that branch into pi1 or mathlib
 theorem Etale.iff_locally_isStandardSmoothOfRelativeDimension
     {R S : Type u} [CommRing R] [CommRing S] (f : R →+* S) :
-    f.Etale ↔ Locally (IsStandardSmoothOfRelativeDimension 0) f :=
-  sorry
+    f.Etale ↔ Locally (IsStandardSmoothOfRelativeDimension 0) f := by
+  rw [etale_iff_formallyUnramified_and_smooth, ← locally_isStandardSmooth_iff_smooth]
+  refine ⟨fun ⟨hr, hs⟩ ↦ ?_, fun h ↦ ⟨?_, ?_⟩⟩
+  · have : Locally (fun f ↦ f.FormallyUnramified ∧ f.IsStandardSmooth) f := by
+      obtain ⟨s, hs, H⟩ := hs
+      refine ⟨s, hs, fun t ht ↦ ⟨?_, H t ht⟩⟩
+      exact
+        FormallyUnramified.propertyIsLocal.StableUnderCompositionWithLocalizationAwayTarget _ t _ hr
+    refine locally_of_locally ?_ this
+    intro R S _ _ f hf
+    algebraize [f]
+    have : Algebra.FormallyUnramified R S := hf.1
+    have : Algebra.IsStandardSmooth R S := hf.2
+    show Algebra.IsStandardSmoothOfRelativeDimension 0 R S
+    infer_instance
+  · rw [← locally_iff_of_localizationSpanTarget FormallyUnramified.respectsIso
+      FormallyUnramified.ofLocalizationSpanTarget]
+    refine locally_of_locally ?_ h
+    intro R S _ _ f hf
+    algebraize [f]
+    have : Algebra.IsStandardSmoothOfRelativeDimension 0 R S := hf
+    show Algebra.FormallyUnramified R S
+    infer_instance
+  · refine locally_of_locally ?_ h
+    intro R S _ _ f hf
+    exact hf.isStandardSmooth
 
 end RingHom
