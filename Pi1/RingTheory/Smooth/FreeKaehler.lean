2fd40aa096896be26c1f9a18e129068ec6300e78
Christian Merten <christian@merten.dev>
2025-05-11 20:38:53 +0200

    core of presentation api

--- Pi1/RingTheory/Smooth/FreeKaehler.lean
+++ Pi1/RingTheory/Smooth/FreeKaehler.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christian Merten
 -/
 import Pi1.RingTheory.Smooth.KaehlerLocalization
+import Pi1.Mathlib.RingTheory.Presentation
 import Mathlib.RingTheory.Smooth.StandardSmooth
 import Mathlib.RingTheory.Smooth.StandardSmoothCotangent
 import Mathlib.RingTheory.Etale.Kaehler
@@ -395,56 +396,6 @@ lemma cotangentRestrict_bijective_of_basis_kaehlerDifferential [Subsingleton (H1
       P.cotangentSpaceBasis v hv u hu P.toExtension.exact_cotangentComplex_toKaehler ?_ huv
     · rw [heq]
       exact b.span_eq
-
-lemma aeval_mk_X_eq_mk {σ : Type*} (I : Ideal (MvPolynomial σ R)) :
-    aeval (fun i ↦ Ideal.Quotient.mk I (X i)) = Ideal.Quotient.mkₐ _ I := by
-  rw [aeval_unique (Ideal.Quotient.mkₐ _ I)]
-  rfl
-
-def Generators.naive {σ : Type t₁} {ι : Type t₂} (v : ι → MvPolynomial σ R)
-    (s : MvPolynomial σ R ⧸ (Ideal.span <| Set.range v) → MvPolynomial σ R := Quotient.out)
-    (hs : ∀ x, Quotient.mk'' (s x) = x := by simp) :
-    Generators.{t₁} R (MvPolynomial σ R ⧸ (Ideal.span <| Set.range v)) where
-  vars := σ
-  val i := Ideal.Quotient.mk _ (X i)
-  σ' := s
-  aeval_val_σ' x := by
-    rw [aeval_mk_X_eq_mk]
-    apply hs
-  algebra := inferInstance
-  algebraMap_eq := by
-    ext x
-    · simp only [RingHom.coe_comp, Function.comp_apply, RingHom.coe_coe, algHom_C]
-      rfl
-    · simp
-
-lemma Generators.naive_val {σ ι : Type*} (v : ι → MvPolynomial σ R)
-    (s : MvPolynomial σ R ⧸ (Ideal.span <| Set.range v) → MvPolynomial σ R := Quotient.out)
-    (hs : ∀ x, Quotient.mk'' (s x) = x := by simp) (i : σ) :
-    (Generators.naive v s hs).val i = Ideal.Quotient.mk _ (X i) :=
-  rfl
-
-lemma Generators.naive_ker {σ ι : Type*} (v : ι → MvPolynomial σ R)
-    (s : MvPolynomial σ R ⧸ (Ideal.span <| Set.range v) → MvPolynomial σ R)
-    (hs : ∀ x, Quotient.mk'' (s x) = x) :
-    (Generators.naive v s hs).ker = Ideal.span (Set.range v) :=
-  (Ideal.span (Set.range v)).mk_ker
-
-lemma Generators.naive_σ {σ ι : Type*} (v : ι → MvPolynomial σ R)
-    (s : MvPolynomial σ R ⧸ (Ideal.span <| Set.range v) → MvPolynomial σ R)
-    (hs : ∀ x, Quotient.mk'' (s x) = x)
-    (x : MvPolynomial σ R ⧸ (Ideal.span <| Set.range v)) :
-    (Generators.naive v s hs).σ x = s x :=
-  rfl
-
-def Presentation.naive {σ : Type t₁} {ι : Type t₂} (v : ι → MvPolynomial σ R)
-    (s : MvPolynomial σ R ⧸ (Ideal.span <| Set.range v) → MvPolynomial σ R := Quotient.out)
-    (hs : ∀ x, Quotient.mk'' (s x) = x := by simp) :
-    Presentation.{t₂, t₁} R (MvPolynomial σ R ⧸ (Ideal.span <| Set.range v)) where
-  __ := Generators.naive v s hs
-  rels := ι
-  relation := v
-  span_range_relation_eq_ker := (Generators.naive_ker v s hs).symm
 
 open _root_.TensorProduct
 
